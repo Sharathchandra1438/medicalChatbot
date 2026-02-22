d335a19004680c5bcf99ccd3347657dbf8993bb9
Sharath Chandra <sharathchandra7060@gmail.com>
2026-02-22 11:21:58 +0530

    requirementsAdded

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name='medicalChatbot',
+    version='0.1.0',
+    author='Sharath',
+    author_email='sharathchandra7060@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
